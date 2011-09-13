842568a7b1087b89205e301707dfeae89d49e8c6
Sarah Zelechoski <szelechoski@sourcefire.com>
2011-09-13 16:44:35 -0400

    rearrange some variables

--- lib/capistrano/git/notifier.rb
+++ lib/capistrano/git/notifier.rb
@@ -28,15 +28,16 @@ Capistrano::Configuration.instance.load do
 			deployer = "#{user} (#{email})"
 			source_repo = `git config --get remote.origin.url`.split(':')[1].split('.')[0]
 			application_name = `git config --get remote.origin.url`.strip.split('/').last.split('.').first
-			tags = `git tag -l`.split(/\n/).reverse
 			
-			deployed_version = "#{tag_format(:release => release_name)}"
-			deployed = tags[1]
-			deploying = tags[0]
-			compare_url = "http://github.com/#{source_repo}/compare/#{deployed}...#{deploying}"
       
 		  desc 'Alert Campfire of a deploy'
 		  task :campfire do
+
+				tags = `git tag -l`.split(/\n/).reverse
+				deployed_version = "#{tag_format(:release => release_name)}"
+				deployed = tags[1]
+				deploying = tags[0]
+				compare_url = "http://github.com/#{source_repo}/compare/#{deployed}...#{deploying}"
 
 				campfire = Tinder::Campfire.new 'pig', :token => 'f40051873e877ad46c2adcf59902174e7fab0376'
 				room = campfire.find_room_by_name("The Web Team")
@@ -54,6 +55,13 @@ Capistrano::Configuration.instance.load do
 		
 			desc 'Alert e-mail lists of a deploy'
 			task :mailer do
+			
+				tags = `git tag -l`.split(/\n/).reverse
+				deployed_version = "#{tag_format(:release => release_name)}"
+				deployed = tags[1]
+				deploying = tags[0]
+				compare_url = "http://github.com/#{source_repo}/compare/#{deployed}...#{deploying}"
+				
 				release_notes = File.read(rails_root + "/config/CHANGELOG")
 				body = ""
 				body << "----------------------------------------------------------------\n"
