2d716e1e23cfdd14a74c98cd4d69802405e434dd
Sarah Zelechoski <szelechoski@sourcefire.com>
2011-09-13 15:45:17 -0400

    reformat mail in notifier

--- lib/capistrano/git/notifier.rb
+++ lib/capistrano/git/notifier.rb
@@ -13,22 +13,23 @@ Capistrano::Configuration.instance.load do
   namespace :git do
 
 		namespace :notifier do
+
+			branch_name = `git branch`.split(/\s/)[1]
+			user = `git config --get user.name`
+			email = `git config --get user.email`
+			deployer = "#{user} (#{email})"
+			source_repo = `git config --get remote.origin.url`.split(':')[1].split('.')[0]
+			application_name = `git config --get remote.origin.url`.strip.split('/').last.split('.').first
+			tags = `git tag -l`.split(/\n/).reverse
+			
+			deployed_version = "#{tag_format(:release => release_name)}"
+			deployed = tags[1]
+			deploying = tags[0]
+			compare_url = "http://github.com/#{source_repo}/compare/#{deployed}...#{deploying}"
       
 		  desc 'Alert Campfire of a deploy'
 		  task :campfire do
-		
-		    branch_name = `git branch`.split(/\s/)[1]
-		    user = `git config --get user.name`
-        email = `git config --get user.email`
-		    deployer = "#{user} (#{email})"
-		    source_repo = `git config --get remote.origin.url`.split(':')[1].split('.')[0]
-		    application_name = `git config --get remote.origin.url`.strip.split('/').last.split('.').first
-		    tags = `git tag -l`.split(/\n/).reverse
-		    
-		    deployed = tags[1]
-		    deploying = tags[0]
-		    compare_url = "http://github.com/#{source_repo}/compare/#{deployed}...#{deploying}"
-		
+
 				campfire = Tinder::Campfire.new 'pig', :token => 'f40051873e877ad46c2adcf59902174e7fab0376'
 				room = campfire.find_room_by_name("The Web Team")
 				room.speak "#{deployer} deployed "
@@ -46,7 +47,19 @@ Capistrano::Configuration.instance.load do
 			desc 'Alert e-mail lists of a deploy'
 			task :mailer do
 				release_notes = File.read(rails_root + "/config/CHANGELOG")
-				Pony.mail(:to => 'sfweb@sourcefire.com', :via => :sendmail, :body => release_notes)
+				body = ""
+				body << "----------------------------------------------------------------\n"
+				body << "Version: #{deployed_version.strip}\n"
+	      body << "Deployed: #{Date.today} at #{Time.now}\n"
+	      body << "Contact: #{user.strip} (#{email.strip})\n"
+	      body << "----------------------------------------------------------------\n"
+	      body << release_notes.gsub(/^ */, ' ' * 4)
+	      body << "\n----------------------------------------------------------------\n\n"
+				      
+				Pony.mail(:to => 'sfweb@sourcefire.com', 
+				:via => :sendmail, 
+				:subject => "deployment notice: #{branch_name} branch of #{application_name} to #{rails_env}" ,
+				:body => body)
 			end
 		end
 	end
